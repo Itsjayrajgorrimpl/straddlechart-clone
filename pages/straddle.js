360e690f8964ac926f3ec05f0f8541c77c9cb007
Itsjayrajgorrimpl <jayrajgorr@gmail.com>
2026-02-22 15:27:30 +0530

    Create straddle.js

---
+++ pages/straddle.js
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar"
+import StraddleChart from "../components/StraddleChart"
+
+export default function Straddle(){
+
+ return(
+
+ <div>
+
+ <Navbar/>
+
+ <h2>ATM Straddle</h2>
+
+ <StraddleChart/>
+
+ </div>
+
+ )
+
+}
