5ec12332a9cb5d33f3fa86d59806b180b5f857a6
Itsjayrajgorrimpl <jayrajgorr@gmail.com>
2026-02-22 15:27:56 +0530

    Create strangle.js

---
+++ pages/strangle.js
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar"
+import StrangleChart from "../components/StrangleChart"
+
+export default function Strangle(){
+
+ return(
+
+ <div>
+
+ <Navbar/>
+
+ <h2>OTM Strangle</h2>
+
+ <StrangleChart/>
+
+ </div>
+
+ )
+
+}
